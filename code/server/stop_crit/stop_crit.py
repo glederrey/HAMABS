05519ab1e60600bd2f80e3437cc5a9df21a13f9a
glederrey <gael.lederrey@epfl.ch>
2019-08-09 18:54:13 +0200

    [FIX] issue with stop crit test

--- code/server/stop_crit/stop_crit.py
+++ code/server/stop_crit/stop_crit.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../..")
 
 import os
-import time
+import numpy as np
 import json
 
 from algos import OptAlg
@@ -10,6 +10,22 @@ from algos import OptAlg
 from models import LPMC_Full
 
 data_folder = '../../../data/'
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
 
 if __name__ == "__main__":
 
@@ -22,7 +38,11 @@ if __name__ == "__main__":
 
     ioa = OptAlg(alg_type='LS-ABS', direction='hybrid-inv')
 
-    res = model.optimize(ioa, **{'verbose': False, 'nbr_epochs': 1000, 'batch': 1000})
+    res = model.optimize(ioa, **{'verbose': False, 'nbr_epochs': 1000, 'batch': 100})
+
+    print(res)
+
+    dumped = json.dumps(res, cls=NumpyEncoder)
 
     with open('results/res.json', 'w') as outfile:
-        json.dump(res, outfile)
+        json.dump(dumped, outfile)
