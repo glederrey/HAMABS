2eae24d761c9b91acaf08665977529bc5c805c66
glederrey <gael.lederrey@epfl.ch>
2019-11-28 10:30:22 +0100

    [ADD] Final results from server

--- code/server/parameters/RR_hybrid.py
+++ code/server/parameters/RR_hybrid.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     base_param = {'perc_hybrid': 30, 'thresh_upd': 1, 'count_upd': 2, 'window': 10, 'factor_upd': 2}
 
-    main_params = {'verbose': True, 'nbr_epochs': 1000, 'batch': 1000}
+    main_params = {'verbose': False, 'nbr_epochs': 1000, 'batch': 1000}
     main_params.update(base_param)
 
     draws = 10
